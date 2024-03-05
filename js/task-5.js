841d59abbbf9ef502c8460d0dff61c023af8a4de
waytPrime <73542400+Carolinealt@users.noreply.github.com>
2024-03-05 13:18:57 +0200

    5

--- js/task-5.js
+++ js/task-5.js
@@ -4,21 +4,37 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const refs = {
-  color: document.querySelector(".color"),
-  btnChangeColor: document.querySelector(".change-color"),
-  body: document.querySelector('body')
-};
+const button = document.querySelector(".change-color");
 
-function changeColorBackground(){
-  refs.btnChangeColor.addEventListener('click', changeColorSpan)
-  
+button.addEventListener("click", changeClr);
+
+function changeClr(e) {
+  const color = getRandomHexColor();
+  document.querySelector(".color").textContent = color;
+  document.querySelector("body").style.backgroundColor = color;
 }
 
-function changeColorSpan(event) {
-  refs.body.style.backgroundColor = getRandomHexColor();
-  refs.color.textContent = `${refs.body.style.backgroundColor}`
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
-}
+// const refs = {
+//   color: document.querySelector(".color"),
+//   btnChangeColor: document.querySelector(".change-color"),
+//   body: document.querySelector('body')
+// };
 
-changeColorBackground();
+// function changeColorBackground(){
+//   refs.btnChangeColor.addEventListener('click', changeColorSpan)
+
+// }
+
+// function changeColorSpan(event) {
+//   refs.body.style.backgroundColor = getRandomHexColor();
+//   refs.color.textContent = `${refs.body.style.backgroundColor}`
+
+// }
+
+// changeColorBackground();
