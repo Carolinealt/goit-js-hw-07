90b6370d29fa12e0579a62d4d58b19d494724a3e
waytPrime <73542400+Carolinealt@users.noreply.github.com>
2024-03-05 12:48:04 +0200

    =

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,43 @@
 const form = document.querySelector(".login-form");
-
-form.addEventListener("submit", submitForm);
-
-function submitForm(event) {
-  event.preventDefault();
-
-  const emailValue = event.currentTarget.elements.email.value;
-  const passwordValue = event.currentTarget.elements.password.value;
-
-  if (emailValue === "" || passwordValue === "") {
+const refs = {
+  email: document.querySelector('input[type="email"]'),
+  password: document.querySelector('input[type="password"]'),
+};
+const dataForm = {
+  email: null,
+  password: null,
+};
+form.addEventListener("submit", sendForm);
+function sendForm(e) {
+  e.preventDefault();
+  if (refs.email.value.trim() === "" || refs.password.value.trim() === "") {
     window.alert("All form fields must be filled in");
     return;
   }
-
-  const formData = new FormData(event.currentTarget);
   
-
-
-  // formData.forEach((value, name) => {
-  //   console.log({ name, value });
-  // });
-  form.reset();
+  dataForm.email = e.target.elements.email.value;
+  dataForm.password = e.target.elements.password.value;
 }
+
+// const form = document.querySelector(".login-form");
+
+// form.addEventListener("submit", submitForm);
+
+// function submitForm(event) {
+//   event.preventDefault();
+
+//   const emailValue = event.currentTarget.elements.email.value;
+//   const passwordValue = event.currentTarget.elements.password.value;
+
+//   if (emailValue === "" || passwordValue === "") {
+//     window.alert("All form fields must be filled in");
+//     return;
+//   }
+
+//   const formData = new FormData(event.currentTarget);
+
+//   // formData.forEach((value, name) => {
+//   //   console.log({ name, value });
+//   // });
+//   form.reset();
+// }
